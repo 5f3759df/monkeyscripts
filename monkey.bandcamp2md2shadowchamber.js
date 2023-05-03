777ce099582564aa8fe0f2c56dfe297d46581b44
Pawel <hades@webrush.pl>
2023-05-03 19:06:12 +0200

    Cosmetic changes

--- monkey.bandcamp2md2shadowchamber.js
+++ monkey.bandcamp2md2shadowchamber.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bandcamp.com/album
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @match        https://*.bandcamp.com/album/*
@@ -11,25 +11,11 @@
 
 (function() {
     'use strict';
-
-    //alert('Im in!!');
     const scurl = location.protocol + '//' + location.host + location.pathname;
-    /* mobile
-    const scartist = document.querySelector('section#purchase-options div.carousel div.carousel-item div.purchase-card.digital div.body span.attribution');
-    const scalbum = document.querySelector('section#purchase-options div.carousel div.carousel-item div.purchase-card.digital div.body h2.name');
-    const scdescription = document.querySelector('main#p-tralbum-page.dark div#p-tralbum-page-inner section#about-tralbum.description p.expando-text.not-truncatable');
-    const screleased = document.querySelector('main#p-tralbum-page.dark div#p-tralbum-page-inner div#tralbum-info.add-border section p.release-date');
-    const sccredits = document.querySelector('main#p-tralbum-page.dark div#p-tralbum-page-inner div#tralbum-info.add-border section div.credits p.expando-text.not-truncatable');
-    const sctagsarray = [...document.querySelectorAll('main#p-tralbum-page.dark div#p-tralbum-page-inner div#tralbum-info.add-border section a.tag')];
-    */
     const scartist = document.querySelector('div#centerWrapper div#propOpenWrapper div.trackView.has-art div#name-section h3 span a');
     const scalbum = document.querySelector('div#name-section h2.trackTitle');
     const scdescription = document.querySelector('div#centerWrapper div#propOpenWrapper div.trackView div#trackInfo div#trackInfoInner div.tralbumData.tralbum-about');
-
-
-
     const sccreditsfull = document.querySelector('div#centerWrapper div#propOpenWrapper div.trackView div#trackInfo div#trackInfoInner div.tralbumData.tralbum-credits');
-    //console.log(sccreditsfull);
     let screleased = null;
     let sccredits = null;
     if(sccreditsfull){
@@ -58,11 +44,9 @@ ${scdescription.textContent.trim()}`:'')}
 
     document.querySelector('body').addEventListener('click', e =>{
         console.log(description);
-        navigator.clipboard.writeText(description).then(() => {
-            /* clipboard successfully set */
-            console.log('ok');
-        }, () => {
-            /* clipboard write failed */
+        navigator.clipboard.writeText(description)
+            .then(() => { console.log('ok');},
+                  (err) => { console.log('error: ',err);
         });
     });
 })();
